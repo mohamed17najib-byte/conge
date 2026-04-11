c9fa8cd3799e57ce75eeb9bc11627dbcddb2abe7
MohamedNajibDev <mohamed.najib.engineer@gmail.com>
2026-04-11 19:01:56 +0000

    add location

--- api/geo.ts
+++ api/geo.ts
@@ -1,6 +1,14 @@
+// api/geo.ts
 export default async function handler(req: any, res: any) {
-  const response = await fetch("https://ipapi.co/json/");
+  // Récupère l'IP réelle du visiteur
+  const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.socket?.remoteAddress || '';
+  
+  const response = await fetch(`https://freeipapi.com/api/json/${ip}`);
   const data = await response.json();
+  
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.json(data);
+  res.json({
+    country_code: data.countryCode,
+    country_name: data.countryName,
+  });
 }
